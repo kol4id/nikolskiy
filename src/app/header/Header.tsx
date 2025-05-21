f1b8d2a20e09a5439502f22054038aa36a2c315b
Kirill Nagulin <over90006at9@gmail.com>
2025-05-21 22:25:35 +0300

    Add logo and phone number display

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -1,4 +1,5 @@
 import styles from './Header.module.scss'
+import logo from '@/assets/Logotype Nikolski.svg'
 
 const Header = () =>{
     return(
@@ -9,6 +10,10 @@ const Header = () =>{
                     <a href='#services'>О нас</a>
                     <a href='#services'>Контакты</a>
                 </nav>
+                <img loading='lazy' src={logo}></img>
+                <div className={styles.phone}>
+                    <a href="tel:+79015184400">+7 (901) 518 44 00</a>
+                </div>
             </header>
         </>
     )
